f53f915d2e6065217a0dee752733c1861a4925bc
livnas <119596635+livnas@users.noreply.github.com>
2022-12-01 13:17:17 +0100

    Java - Advent of Code 2022, day 1

---
+++ AoC2022d1/src/day1.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class day1 {
+    public static void main(String[] args) {
+
+        int dataV;
+        int value = 0;
+
+        ArrayList<Integer> valueList = new ArrayList<Integer>(); // Create an ArrayList object
+
+        try {
+            File calFile = new File("values.txt");
+
+            Scanner reader = new Scanner(calFile);
+
+            while (reader.hasNextLine())
+            {
+
+                String data = reader.nextLine();
+
+                if(!data.equals(""))
+                {
+                    dataV = Integer.parseInt(data);
+                    value += dataV;
+                }
+                else if (data.equals(""))
+                {
+                    valueList.add(value);
+                    value = 0;
+                }
+
+            }
+
+            Collections.sort(valueList);
+
+            System.out.println(valueList.get(valueList.size() - 1) + valueList.get(valueList.size() - 2) + valueList.get(valueList.size() - 3));
+
+
+            reader.close();
+
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
